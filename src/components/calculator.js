492f5c7872502b15503c88d5084de93d37868977
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-11-01 23:45:06 +0500

    Add styling to the calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,27 +1,27 @@
 const buttons = () => (
-  <section className="calculatorContainer">
+  <section className="calculatorContainer flex">
     <div className="display">0</div>
     <div className="calculator">
       <ul className="buttons">
-        <li className="simpleButtons">AC</li>
-        <li className="simpleButtons">+/-</li>
-        <li className="simpleButtons">%</li>
-        <li className="colorButtons">/</li>
-        <li className="simpleButtons">7</li>
-        <li className="simpleButtons">8</li>
-        <li className="simpleButtons">9</li>
-        <li className="colorButtons">X</li>
-        <li className="simpleButtons">4</li>
-        <li className="simpleButtons">5</li>
-        <li className="simpleButtons">6</li>
-        <li className="colorButtons">-</li>
-        <li className="simpleButtons">1</li>
-        <li className="simpleButtons">2</li>
-        <li className="simpleButtons">3</li>
-        <li className="colorButtons">+</li>
-        <li className="bigButton">0</li>
-        <li className="simpleButtons">.</li>
-        <li className="colorButtons">=</li>
+        <li className="simpleButtons flex">AC</li>
+        <li className="simpleButtons flex">+/-</li>
+        <li className="simpleButtons flex">%</li>
+        <li className="colorButtons flex">/</li>
+        <li className="simpleButtons flex">7</li>
+        <li className="simpleButtons flex">8</li>
+        <li className="simpleButtons flex">9</li>
+        <li className="colorButtons flex">X</li>
+        <li className="simpleButtons flex">4</li>
+        <li className="simpleButtons flex">5</li>
+        <li className="simpleButtons flex">6</li>
+        <li className="colorButtons flex">-</li>
+        <li className="simpleButtons flex">1</li>
+        <li className="simpleButtons flex">2</li>
+        <li className="simpleButtons flex">3</li>
+        <li className="colorButtons flex">+</li>
+        <li className="bigButton flex">0</li>
+        <li className="simpleButtons flex">.</li>
+        <li className="colorButtons flex">=</li>
       </ul>
 
     </div>
